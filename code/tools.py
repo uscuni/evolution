bce87470a5e8be3151844e8cf729e06de6e318db
Martin Fleischmann <martin@martinfleischmann.net>
2025-12-11 11:07:51 +0100

    edit figures, peek into morphometrics

--- code/tools.py
+++ code/tools.py
@@ -5,6 +5,29 @@ import numpy as np
 import seaborn as sns
 import matplotlib.cm as cm
 
+taxonomy_colors = [
+    "#4069BC",
+    "#7CBAE4",
+    "#E69C63",
+    "#eec1d5",
+    "#E0665F",
+    "#ECBF43",
+    "#b2cd32",
+    "#1F943E",
+]
+taxonomy_cmap = {i: col for i, col in enumerate(taxonomy_colors, 1)}
+
+names = {
+    1: "Incoherent Large-Scale Homogeneous Fabric",
+    2: "Incoherent Large-Scale Heterogeneous Fabric",
+    3: "Incoherent Small-Scale Linear Fabric",
+    4: "Incoherent Small-Scale Sparse Fabric",
+    5: "Incoherent Small-Scale Compact Fabric",
+    6: "Coherent Interconnected Fabric",
+    7: "Coherent Dense Disjoint Fabric",
+    8: "Coherent Dense Adjacent Fabric",
+}
+
 def pivot_tbl(df,lvl):
     grouped = (
         df
@@ -43,30 +66,53 @@ def pivot_tbl_cum(df,lvl):
 
 
 def line_plot(table):
-    n_categories = len(table.columns)
-    cmap = cm.get_cmap("tab20b", n_categories)
-    colors = [cmap(i) for i in range(n_categories)]
-    
-    plt.figure(figsize=(12, 6))
-    
+    f, ax = plt.subplots(figsize=(12, 6))
+    n_categories = table.shape[1]
+    if n_categories <= 8:
+        color = taxonomy_cmap
+    else:
+        cmap = cm.get_cmap("tab20b", n_categories)
+        color = [cmap(i) for i in range(n_categories)]
+        
     for i, col in enumerate(table.columns):
-        plt.plot(
+        ax.plot(
             table.index.astype(str),  # convert intervals to string for x-axis
             table[col],
             label=str(col),
-            color=colors[i],
+            color=color[i + 1] if n_categories <= 8 else color[i],
             marker='o'
         )
     
-    plt.xlabel("Year Mode")
-    plt.ylabel("Proportion")
-    plt.xticks(rotation=45)
-    plt.legend(bbox_to_anchor=(1.05, 1))
+    ax.set_xlabel("Year Mode")
+    ax.set_ylabel("Proportion")
+    plt.xticks(rotation=90)
+    ax.legend(bbox_to_anchor=(1.05, 1))
+    handles, labels = ax.get_legend_handles_labels()
+    if table.shape[1] == 8:
+        legend = [names[int(label)] for label in labels]
+    else:
+        legend = labels
+    ax.legend(handles, legend,
+              bbox_to_anchor=(1.01, 1), loc='upper left', frameon=False)
+    sns.despine()
     plt.tight_layout()
+    
 
-def hist (table): 
-    n_categories = len(table.columns)
-    cmap = cm.get_cmap("tab20b", n_categories)  # can also try "tab20c", "tab20", "hsv"
-    colors = [cmap(i) for i in range(n_categories)]
-    table.plot(kind="bar", stacked=True, color=colors, figsize=(12,6))
-    plt.legend(bbox_to_anchor=(1.05, 1))
+def hist(table): 
+    f, ax = plt.subplots(figsize=(12, 6))
+    if table.shape[1] <= 8:
+        color = taxonomy_cmap
+    else:
+        cmap = plt.get_cmap("tab20b", table.shape[1])
+        color = [cmap(i) for i in range(table.shape[1])]
+    table.plot.bar(stacked=True, color=color, ax=ax, width=1)
+    ax.legend(bbox_to_anchor=(1.05, 1))
+    # Rename legend using type_names
+    handles, labels = ax.get_legend_handles_labels()
+    if table.shape[1] == 8:
+        legend = [names[int(label)] for label in labels]
+    else:
+        legend = labels
+    ax.legend(handles, legend,
+              bbox_to_anchor=(1.01, .98), loc='upper left', frameon=False)
+    sns.despine()
